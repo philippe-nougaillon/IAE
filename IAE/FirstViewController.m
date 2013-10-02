de37f01870294f3f9c7bb20dad6be5c21bc9bb8b
Philippe Nougaillon <phil@dhcp-bro-re81.univ-paris1.fr>
2013-10-02 10:49:32 +0200

    planning json parsing

--- IAE/FirstViewController.m
+++ IAE/FirstViewController.m
@@ -18,6 +18,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://entiae.univ-paris1.fr/hyperjson/"]];
+    NSURLResponse *response;
+    NSError *error;
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+
+    if (data != nil) {
+        NSLog(@"OK");
+    } else {
+        if (error != nil)
+            NSLog(@"Echec connection (%@)", [error localizedDescription]);
+        else
+            NSLog(@"Echec cde la onnection");
+    }
+    
+    NSError *errorDecoding;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&errorDecoding];
+    //NSLog(@"%@",json);
+    
+    for (NSDictionary *obj in json) {
+        NSString *heure = [obj objectForKey:@"heure"];
+        NSString *matiere = [obj objectForKey:@"matiere"];
+        NSString *enseignant = [obj objectForKey:@"enseignant"];
+        NSString *memo = [obj objectForKey:@"memo"];
+        NSString *salle = [obj objectForKey:@"salle"];
+        NSString *tdoptions = [obj objectForKey:@"tdoptions"];
+        NSLog(@"%@ %@ %@ %@ Avec:%@ Salle:%@", heure, matiere, memo, tdoptions, enseignant, salle);
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
