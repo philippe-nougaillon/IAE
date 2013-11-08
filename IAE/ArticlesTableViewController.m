30809b22274c843e5799d322ebf6de9f723c8cd3
Philippe Nougaillon <phil@dhcp-bro-re110.univ-paris1.fr>
2013-11-08 16:58:37 +0100

    0.6

--- IAE/ArticlesTableViewController.m
+++ IAE/ArticlesTableViewController.m
@@ -35,7 +35,7 @@
     [super viewDidLoad];
 
     // Uncomment the following line to preserve selection between presentations.
-    self.clearsSelectionOnViewWillAppear = NO;
+    //self.clearsSelectionOnViewWillAppear = NO;
  
     // change navigation bar background
     [self.navigationController.navigationBar
